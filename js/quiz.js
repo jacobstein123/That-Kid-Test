ec210aef015a844ea780e896ef8be008fabac3ab
jacobstein123 <jacobstein123@gmail.com>
2018-02-27 22:11:52 -0600

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,27 +1,54 @@
-var json = {pages: [
-    {questions: [
-        { type: "matrix",
-            columns: [{ value: 1, text: "Never" },
-                { value: 2, text: "Rarely" },
-                { value: 3, text: "Sometimes" },
-                { value: 4, text: "Often" },
-                { value: 5, text: "Always" }],
-            rows: [{ value: "affordable", text: "Product is affordable" },
-                { value: "does what it claims", text: "Product does what it claims" },
-                { value: "better then others", text: "Product is better than other products on the market" },
-                { value: "easy to use", text: "Product is easy to use" }]}
-        
-    ]}
-]};
+var json = {
+    questions: [
+        {
+            type: "matrix",
+            name: "Quality",
+            title: "Please indicate if you agree or disagree with the following statements",
+            columns: [
+                {
+                    value: 1,
+                    text: "Strongly Disagree"
+                }, {
+                    value: 2,
+                    text: "Disagree"
+                }, {
+                    value: 3,
+                    text: "Neutral"
+                }, {
+                    value: 4,
+                    text: "Agree"
+                }, {
+                    value: 5,
+                    text: "Strongly Agree"
+                }
+            ],
+            rows: [
+                {
+                    value: "affordable",
+                    text: "Product is affordable"
+                }, {
+                    value: "does what it claims",
+                    text: "Product does what it claims"
+                }, {
+                    value: "better then others",
+                    text: "Product is better than other products on the market"
+                }, {
+                    value: "easy to use",
+                    text: "Product is easy to use"
+                }
+            ]
+        }
+    ]
+};
 
 Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
 Survey.Survey.cssType = "bootstrap";
 
 var model = new Survey.Model(json);
-model.onComplete.add(function(result) {
-  console.log(JSON.stringify(result.data));
-});
+survey
+    .onComplete
+    .add(function (result) {
+        console.log("result: " + JSON.stringify(result.data));
+    });
 
-$("#surveyElement").Survey({model:model
-                            data: data
-                           });
+$("#surveyElement").Survey({model:model});
