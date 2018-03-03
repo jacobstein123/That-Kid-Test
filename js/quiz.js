cf9fc043501830be3cf262b8765cf927d562e891
jacobstein123 <jacobstein123@gmail.com>
2018-03-03 16:26:09 -0600

    Implemented data collection!

--- js/quiz.js
+++ js/quiz.js
@@ -169,23 +169,6 @@ var json2 = {
     completedHtml:" "
 };
 
-function myIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-    xmlhttp.send();
-
-    hostipInfo = xmlhttp.responseText.split("\n");
-
-    for (i=0; hostipInfo.length >= i; i++) {
-        ipAddress = hostipInfo[i].split(":");
-        if ( ipAddress[0] == "IP" ) return ipAddress[1];
-    }
-
-    return false;
-}
-
 var model2 = new Survey.Model(json2);
 model2
     .onComplete
@@ -194,7 +177,7 @@ model2
         console.log("result1: " + JSON.stringify(result1.data));
         console.log("result2: " + JSON.stringify(result2.data));
 
-        var results_string = JSON.stringify(result1.data) + " " + JSON.stringify(result2.data) + " " + myIP();
+        var results_string = JSON.stringify(result1.data) + " " + JSON.stringify(result2.data);
 
         var tkness = 0.0;
         var tkness_count = 0;
@@ -408,14 +391,18 @@ model2
         }
         document.getElementById("intro1").innerHTML = "";
 
-        document.getElementById("intro2_div").style.display = "block";
+        document.getElementById("intro2_div").style.display = "none";
         document.getElementById('portfolio').scrollIntoView(true);
 
+        
         var sendData = document.getElementById("sendData");
         sendData.style.display = "inline-block";
-        document.getElementById("dataToSend").value = results_string;
+        document.getElementById("entry.1165881888").value = results_string + " TK percent: " + tkness_percent + 
+            " intelligence_percent: " + intelligence_percent + " hostility_percent: "+hostility_percent 
+            + " peer_percent: " + peer_percent + " faculty_percent: "+faculty_percent;
         document.getElementById("submitData").click(); 
         sendData.style.display = "none";   
+        
     });
 
 $("#surveyElement2").Survey({model:model2});
